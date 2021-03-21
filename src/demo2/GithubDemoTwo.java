62921a82f79232e819ca9b67792838f4aa1e98db
helen <helen@192.168.0.4>
2021-03-21 14:31:53 -0500

    Added println for Demo purpose

--- src/demo2/GithubDemoTwo.java
+++ src/demo2/GithubDemoTwo.java
@@ -4,6 +4,12 @@ public class GithubDemoTwo {
 	
 	public static void main(String[] args) {
 		System.out.println("My first github commit......");
+		
+		gitDemoOne();
 	}
 
+	
+	public static void gitDemoOne() {
+		System.out.println("Pringting gitDemoOne for understanding process");
+	}
 }
