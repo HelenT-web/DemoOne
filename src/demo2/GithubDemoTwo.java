05a35f2fffd25a7a81e7d28bbce8104990274dcb
helen <helen@192.168.0.4>
2021-03-21 14:48:20 -0500

    Added webUI method

--- src/demo2/GithubDemoTwo.java
+++ src/demo2/GithubDemoTwo.java
@@ -1,9 +1,11 @@
 package demo2;
 
 public class GithubDemoTwo {
-	
+
 	public static void main(String[] args) {
 		System.out.println("My first github commit......");
+
+		gitDemoOne();
 		
 		gitDemoOne();
 	}
@@ -11,5 +13,18 @@ public class GithubDemoTwo {
 	
 	public static void gitDemoOne() {
 		System.out.println("Pringting gitDemoOne for understanding process");
+	}
+
+
+	public static boolean webUI() {
+		boolean result = false;
+
+		if("UI".equals("DB")) {
+			result = true;
+		} else {
+			System.out.println("Error Information for UI does not match with DB");
+		}
+		
+		return result;
 	}
 }
